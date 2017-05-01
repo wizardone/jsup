afc3cfde18ba994f86e0d193149f50ea71a7e229
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2017-05-01 16:39:19 +0300

    continue working on the looks of the jsup

--- lib/jsup.rb
+++ lib/jsup.rb
@@ -2,7 +2,22 @@ require "jsup/version"
 
 module Jsup
 
+  attr_reader :attributes
   def self.produce
-    
+    yield self
+  end
+
+  def ojify
+    Oj.dump
+  end
+
+  def new
+    @attributes = Hash.new
+  end
+
+  private
+
+  def method_missing(method, *args)
+    attributes[method] = args
   end
 end
--- spec/jsup_spec.rb
+++ spec/jsup_spec.rb
@@ -1,11 +1,19 @@
 require "spec_helper"
+require 'byebug'
 
 RSpec.describe Jsup do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Jsup::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '.produce' do
+    it 'produces a simple json' do
+      expect(
+        Jsup.produce do|j|
+          j.first 'Stefan'
+          j.email 'stefan@stefan.com'
+        end
+      ).to eq({ first: 'Stefan' }.to_json)
+    end
   end
 end
