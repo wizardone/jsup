4395138e8bff73ff6df20138f1f60ac1159cb33c
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2017-05-03 11:23:29 +0300

    add codecoverage

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in jsup.gemspec
 gemspec
 gem 'byebug', require: false
+gem 'codecov', require: false, group: :test
 gem 'oj'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
 require 'bundler/setup'
 require 'json'
 require 'jsup'
+require 'simplecov'
+
+SimpleCov.start
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
